7e34857d363108efc9cec3209028ee314c125a09
Ankit Singh Chauhan <ankits.chauhan96@gmail.com>
2023-07-30 02:54:14 +0530

    Dummy config values

--- configuration/config.js
+++ configuration/config.js
@@ -1,12 +1,12 @@
 [
     {
-        "channel_name": "playground",
-        "app_name": "none",
-        "webhook_url": "https://hooks.slack.com/services/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"
+        "channel_name": "MyChannel",
+        "app_name": "MyApp",
+        "webhook_url": "https://hooks.slack.com/services/web/hook/url1"
     },
     {
-        "channel_name": "playground",
-        "app_name": "Python Message Sender",
-        "webhook_url": "https://hooks.slack.com/services/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"
+        "channel_name": "MyChannel>",
+        "app_name": "None",
+        "webhook_url": "https://hooks.slack.com/services/web/hook/url2"
     }
 ]
