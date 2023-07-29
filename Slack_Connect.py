57aacd87b5253d3c275fabdd3afe2f0d93f60413
Ankit Singh Chauhan <ankits.chauhan96@gmail.com>
2023-07-30 01:56:38 +0530

    Docstrings Added

--- Slack_Connect.py
+++ Slack_Connect.py
@@ -1,32 +1,40 @@
-from distutils.command.config import config
 import os
 import json
 import requests
 from urllib.parse import urlparse
 import socket
 
-
 class SlackConnect:
+    """
+    SlackConnect class provides functionalities to interact with Slack webhooks and configurations.
+    """
 
-    config_path = '/configuration/config.js'
-    config_status = False
-    config_load = False
-    configuration = []
-    current_config = {}
+    def __init__(self):
+        """
+        Class constructor.
+        Initializes the SlackConnect object with default configuration path, checks for configuration file,
+        and loads the configuration data if found.
+        """
+        self.set_configuration_path()
+        self.check_config()
+        self.get_configuration()
 
-    def set_configuration_path(self, configPath = ''):
-        '''
-        Set Configuration Path for the object
-        '''
-        if configPath != '':
+    def set_configuration_path(self, configPath=''):
+        """
+        Set Configuration Path for the object.
+
+        Parameters:
+        configPath (str): Custom configuration file path. If not provided, the default path is used.
+        """
+        if configPath:
             self.config_path = configPath
         else:
-            self.config_path = self.config_path
+            self.config_path = '/configuration/config.js'
 
     def check_config(self):
-        '''
-        Checking the configuration file
-        '''
+        """
+        Checks the configuration file and sets the config_status accordingly.
+        """
         try:
             with open(os.getcwd() + self.config_path, 'r') as file:
                 content = file.read()
@@ -37,17 +45,16 @@ class SlackConnect:
                 print("Configuration file empty.")
                 self.configuration_setup = []
                 self.config_status = False
-
-            try:
-                json_data = json.loads(content)
-                print("File content is in correct JSON format.")
-                self.configuration_setup = json_data
-                self.config_status = True
-
-            except json.JSONDecodeError as e:
-                print("Error: Invalid Configuration JSON format.")
-                self.configuration_setup = []
-                self.config_status = False
+            else:
+                try:
+                    json_data = json.loads(content)
+                    print("File content is in correct JSON format.")
+                    self.configuration_setup = json_data
+                    self.config_status = True
+                except json.JSONDecodeError as e:
+                    print("Error: Invalid Configuration JSON format.")
+                    self.configuration_setup = []
+                    self.config_status = False
 
         except FileNotFoundError:
             print("Error: Configuration File not found.")
@@ -55,7 +62,9 @@ class SlackConnect:
             self.config_status = False
     
     def get_configuration(self):
-
+        """
+        Loads the configuration data from the configuration file.
+        """
         if self.config_status:
             with open(os.getcwd() + self.config_path, 'r') as file:
                 content = file.read()
@@ -69,7 +78,9 @@ class SlackConnect:
             self.config_load = False
 
     def list_configurations(self):
-
+        """
+        Lists all configurations in a human-readable format.
+        """
         config_values = self.configuration
         config_string = '''
         Channel : {channel_name}
@@ -80,16 +91,24 @@ class SlackConnect:
             webhook_url = value['webhook_url']
             parsed_url = urlparse(webhook_url)
             webhook_url_status = all([parsed_url.scheme, parsed_url.netloc])
-            print(config_string.format(channel_name = value['channel_name']
-                                            ,app_name = value['app_name']
-                                            ,url_status = webhook_url_status))
+            print(config_string.format(channel_name=value['channel_name'],
+                                       app_name=value['app_name'],
+                                       url_status=webhook_url_status))
 
     def initialize_current_config(self):
-
+        """
+        Initializes the current_config with the first configuration in the list.
+        """
         self.current_config = self.configuration[0]
 
-    def select_configuration(self, channel_name, app_name = 'none'):
+    def select_configuration(self, channel_name, app_name='none'):
+        """
+        Selects a specific configuration based on the channel name and app name.
 
+        Parameters:
+        channel_name (str): Channel name to match.
+        app_name (str): App name to match (default: 'none').
+        """
         config_values = self.configuration
         config_set = False
         for value in config_values:
@@ -97,17 +116,20 @@ class SlackConnect:
                 self.current_config = value
                 config_set = True 
 
-        if config_set == False:
+        if not config_set:
             print('Configuration setup Failed!')
     
-    def __init__(self):
-
-        self.set_configuration_path()
-        self.check_config()
-        self.get_configuration()
-
     @classmethod
-    def is_webhook_url_active(cls,url):
+    def is_webhook_url_active(cls, url):
+        """
+        Checks if a webhook URL is active (reachable) without actually sending a request.
+
+        Parameters:
+        url (str): Webhook URL to check.
+
+        Returns:
+        bool: True if the URL is active and reachable, False otherwise.
+        """
         url_parts = url.split("://")[1].split("/", 1)
         host = url_parts[0]
         try:
@@ -128,28 +150,37 @@ class SlackConnect:
             return False  # The URL is not reachable or not active
 
     def check_webhook_url(self):
+        """
+        Checks if the current webhook URL is active.
 
+        Returns:
+        bool: True if the URL is active, False otherwise.
+        """
         webhook_config = self.current_config
         webhook_url = webhook_config['webhook_url']
-        print('Webhook Active : ',self.is_webhook_url_active(webhook_url) )
+        print('Webhook Active : ', self.is_webhook_url_active(webhook_url))
         return True
     
     def send_message(self, message_string):
+        """
+        Sends a message to the current webhook URL.
 
-        payload = '{{"text":"{message_value}"}}'.format(message_value = message_string)
+        Parameters:
+        message_string (str): The message to be sent.
+        """
+        payload = '{{"text":"{message_value}"}}'.format(message_value=message_string)
 
         if self.check_webhook_url():
             try:
                 print(self.current_config['webhook_url'])
                 print(payload)
                 response = requests.post(
-                                            self.current_config['webhook_url'],
-                                            data = payload
-                                        )
+                    self.current_config['webhook_url'],
+                    data=payload
+                )
                 print(response)
                 print('Message Sending completed!')
             except:
                 print('Message Sending failed!')
         else:
             print('Webhook Inactive!')
-
